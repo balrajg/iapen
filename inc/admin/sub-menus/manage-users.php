025590138abcc6df2ae7deae99e505a75f2ff598
balrajg <balrajg@users.noreply.github.com>
2017-09-15 18:11:22 +0530

    user relation functionality create and update

--- inc/admin/sub-menus/manage-users.php
+++ inc/admin/sub-menus/manage-users.php
@@ -87,8 +87,8 @@ class Custom_Table_Example_List_Table extends WP_List_Table {
         // also notice how we use $this->_args['singular'] so in this example it will
         // be something like &person=2
         $actions = array(
-            'edit' => sprintf('<a href="?page=persons_form&id=%s">%s</a>', $item['rel_id'], __('Edit', 'custom_table_example')),
-            'delete' => sprintf('<a href="?page=%s&action=delete&id=%s">%s</a>', $_REQUEST['page'], $item['rel_id'], __('Delete', 'custom_table_example')),
+            'edit' => sprintf('<a href="?page=learn-press-manage-users&id=%s">%s</a>', $item['rel_id'], __('Edit', 'custom_table_example')),
+            'delete' => sprintf('<a href="#">%s</a>', __('Delete', 'custom_table_example')),
         );
 
         return sprintf('%s %s', $item['coursename'], $this->row_actions($actions)
@@ -221,10 +221,10 @@ class Custom_Table_Example_List_Table extends WP_List_Table {
         if ($which == "top") {
             ?>
             <div class="alignleft actions">  <?php
-            //   echo "select u.userlogin as name r.user as user from $table_name as r INNER JOIN $wpdb->users as u on r.user = u.ID ";
-            $users = $wpdb->get_results("select u.user_login as name, r.user as user from $table_name as r INNER JOIN $wpdb->users as u ON r.user = u.ID ", ARRAY_A);
-            if ($users) {
-                ?>
+                //   echo "select u.userlogin as name r.user as user from $table_name as r INNER JOIN $wpdb->users as u on r.user = u.ID ";
+                $users = $wpdb->get_results("select u.user_login as name, r.user as user from $table_name as r INNER JOIN $wpdb->users as u ON r.user = u.ID ", ARRAY_A);
+                if ($users) {
+                    ?>
                     <select name="username-filter" class="username-filter">
                         <option value="">All Users</option>
                         <?php
@@ -254,142 +254,190 @@ class Custom_Table_Example_List_Table extends WP_List_Table {
  * Add-on page
  */
 function learn_press_manage_users_page() {
-    ?>
-    <div id="learn-press-tools-wrap" class="wrap">
-        <h2><?php echo __('Manage Users and Courses', 'learnpress'); ?></h2>
 
-    <?php
-    global $wpdb;
+    //$rel_id = learn_press_get_request("id");
 
-    $table = new Custom_Table_Example_List_Table();
-    $table->prepare_items();
+    if (isset($_REQUEST["id"])) {
 
-    $message = '';
-    if ('delete' === $table->current_action()) {
-        $message = '<div class="updated below-h2" id="message"><p>' . sprintf(__('Items deleted: %d', 'custom_table_example'), count($_REQUEST['id'])) . '</p></div>';
+        learn_press_manage_users_edit($_REQUEST["id"]);
+    } else {
+        ?>
+        <div id="learn-press-tools-wrap" class="wrap">
+            <h2><?php echo __('Manage Users for Courses', 'learnpress'); ?></h2>
+
+            <?php
+            global $wpdb;
+
+            $table = new Custom_Table_Example_List_Table();
+            $table->prepare_items();
+
+            $message = '';
+            if ('delete' === $table->current_action()) {
+                $message = '<div class="updated below-h2" id="message"><p>' . sprintf(__('Items deleted: %d', 'custom_table_example'), count($_REQUEST['id'])) . '</p></div>';
+            }
+            ?>
+            <div class="wrap">
+
+                <div class="icon32 icon32-posts-post" id="icon-edit"><br></div>
+                <h2><?php _e('User Relation', 'custom_table_example') ?> <a class="add-new-h2"
+                                                                            href="<?php echo get_admin_url(get_current_blog_id(), 'admin.php?page=learn-press-manage-users&id=0'); ?>"><?php _e('Add new', 'custom_table_example') ?></a>
+                </h2>
+                <?php echo $message; ?>
+
+                <form id="persons-table" method="GET">
+                    <input type="hidden" name="page" value="<?php echo $_REQUEST['page'] ?>"/>
+                    <?php $table->display() ?>
+                </form>
+
+            </div>
+
+            <?php
+        }
     }
-    ?>
-        <div class="wrap">
 
-            <div class="icon32 icon32-posts-post" id="icon-edit"><br></div>
-            <h2><?php _e('Persons', 'custom_table_example') ?> <a class="add-new-h2"
-                                                                  href="<?php echo get_admin_url(get_current_blog_id(), 'admin.php?page=persons_form'); ?>"><?php _e('Add new', 'custom_table_example') ?></a>
-            </h2>
-    <?php echo $message; ?>
+    function learn_press_manage_users_edit($rel_id) {
+        global $wpdb;
+        $table_name = $wpdb->prefix . 'learnpress_user_relation'; // do not forget about tables prefix
 
-            <form id="persons-table" method="GET">
-                <input type="hidden" name="page" value="<?php echo $_REQUEST['page'] ?>"/>
-    <?php $table->display() ?>
-            </form>
+        $message = '';
+        $notice = '';
 
-        </div>
-
-    <?php
-}
-
-/**
- * PART 4. Form for adding andor editing row
- * ============================================================================
- *
- * In this part you are going to add admin page for adding andor editing items
- * You cant put all form into this function, but in this example form will
- * be placed into meta box, and if you want you can split your form into
- * as many meta boxes as you want
- *
- * http://codex.wordpress.org/Data_Validation
- * http://codex.wordpress.org/Function_Reference/selected
- */
-
-/**
- * Form page handler checks is there some data posted and tries to save it
- * Also it renders basic wrapper in which we are callin meta box render
- */
-function custom_table_example_persons_form_page_handler() {
-    global $wpdb;
-    $table_name = $wpdb->prefix . 'cte'; // do not forget about tables prefix
-
-    $message = '';
-    $notice = '';
-
-    // this is default $item which will be used for new records
-    $default = array(
-        'id' => 0,
-        'name' => '',
-        'email' => '',
-        'age' => null,
-    );
-
-    // here we are verifying does this request is post back and have correct nonce
-    if (wp_verify_nonce($_REQUEST['nonce'], basename(__FILE__))) {
-        // combine our default item with request params
-        $item = shortcode_atts($default, $_REQUEST);
-        // validate data, and if all ok save item to database
-        // if id is zero insert otherwise update
-        $item_valid = custom_table_example_validate_person($item);
-        if ($item_valid === true) {
-            if ($item['id'] == 0) {
-                $result = $wpdb->insert($table_name, $item);
-                $item['id'] = $wpdb->insert_id;
+        // this is default $item which will be used for new records
+        $default = array(
+            'rel_id' => 0,
+            'course_id' => '',
+            'user_role' => '',
+            'parent' => '',
+            'user' => 0
+        );
+        $item['rel_id'] = $_REQUEST["id"];
+        // here we are verifying does this request is post back and have correct nonce
+        if (wp_verify_nonce($_REQUEST['nonce'], basename(__FILE__))) {
+            // combine our default item with request params
+            $item = shortcode_atts($default, $_REQUEST);
+            // validate data, and if all ok save item to database
+            // if id is zero insert otherwise update
+            $item['rel_id'] = $_REQUEST["id"];
+            if ($item['rel_id'] == 0) {
+                $createItem = array(
+                    "course_id" => $item["course_id"],
+                    "user_role" => "",
+                    "parent" => 0,
+                    "user" => $item["user"]
+                );
+                $result = $wpdb->insert($table_name, $createItem);
+                $item['rel_id'] = $rel_id = $wpdb->insert_id;
                 if ($result) {
                     $message = __('Item was successfully saved', 'custom_table_example');
                 } else {
                     $notice = __('There was an error while saving item', 'custom_table_example');
                 }
+//                 echo $wpdb->last_error; 
+//      echo  $wpdb->last_query;
             } else {
-                $result = $wpdb->update($table_name, $item, array('id' => $item['id']));
+                $updateItem = array("parent" => $item["parent"], "user_role" => $item["user_role"]);
+
+                $result = $wpdb->update($table_name, $updateItem, array('rel_id' => $rel_id));
                 if ($result) {
                     $message = __('Item was successfully updated', 'custom_table_example');
                 } else {
                     $notice = __('There was an error while updating item', 'custom_table_example');
                 }
             }
-        } else {
-            // if $item_valid not true it contains error message(s)
-            $notice = $item_valid;
         }
-    } else {
-        // if this is not post back we load item to edit or give new one to create
-        $item = $default;
-        if (isset($_REQUEST['id'])) {
-            $item = $wpdb->get_row($wpdb->prepare("SELECT * FROM $table_name WHERE id = %d", $_REQUEST['id']), ARRAY_A);
+        if ($item['rel_id'] == 0) {
+
+            $args = array(
+                'posts_per_page' => -1,
+                'post_type' => 'lp_course',
+                'post_status' => 'publish',
+                'suppress_filters' => true
+            );
+            $courses_array = get_posts($args);
+            $users_array = get_users();
+        } else {
+
+
+            // if this is not post back we load item to edit or give new one to create
+            $item = $default;
+            //"SELECT relation.rel_id, relation.user_role, user1.user_login as username,  user2.user_login as parent_username, course.post_title as coursename  FROM $table_name as relation INNER JOIN $wpdb->users as user1 ON relation.user = user1.ID LEFT JOIN $wpdb->users as user2 ON relation.parent = user2.ID INNER JOIN $wpdb->posts as course ON course.ID = relation.course_id WHERE rel_id = %d";
+            $item = $wpdb->get_row($wpdb->prepare("SELECT relation.rel_id, relation.parent as parent_id, relation.course_id, relation.user_role, user1.user_login as username,  user2.user_login as parent_username, course.post_title as coursename  FROM $table_name as relation INNER JOIN $wpdb->users as user1 ON relation.user = user1.ID LEFT JOIN $wpdb->users as user2 ON relation.parent = user2.ID INNER JOIN $wpdb->posts as course ON course.ID = relation.course_id WHERE rel_id = %d", $rel_id), ARRAY_A);
             if (!$item) {
                 $item = $default;
-                $notice = __('Item not found', 'custom_table_example');
+                $notice = __('Invalid request', 'custom_table_example');
             }
+            $users = $wpdb->get_results($wpdb->prepare("SELECT usert.user_login as name, relationt.user as user_id, relationt.user_role FROM $table_name as relationt LEFT JOIN $wpdb->users as usert ON  relationt.user = usert.ID WHERE relationt.course_id = %d", $item["course_id"]), ARRAY_A);
         }
-    }
-
-    // here we adding our custom meta box
-    add_meta_box('persons_form_meta_box', 'Person data', 'custom_table_example_persons_form_meta_box_handler', 'person', 'normal', 'default');
-    ?>
-        <div class="wrap">
-            <div class="icon32 icon32-posts-post" id="icon-edit"><br></div>
-            <h2><?php _e('Person', 'custom_table_example') ?> <a class="add-new-h2"
-                                                                 href="<?php echo get_admin_url(get_current_blog_id(), 'admin.php?page=persons'); ?>"><?php _e('back to list', 'custom_table_example') ?></a>
-            </h2>
-
-    <?php if (!empty($notice)): ?>
+        ?><div id="learn-press-tools-wrap" class="wrap">
+            <h2><?php echo __('Manage Users for Courses', 'learnpress'); ?></h2>
+            <?php if (!empty($notice)): ?>
                 <div id="notice" class="error"><p><?php echo $notice ?></p></div>
-    <?php endif; ?>
+            <?php endif; ?>
             <?php if (!empty($message)): ?>
                 <div id="message" class="updated"><p><?php echo $message ?></p></div>
             <?php endif; ?>
-
             <form id="form" method="POST">
                 <input type="hidden" name="nonce" value="<?php echo wp_create_nonce(basename(__FILE__)) ?>"/>
-            <?php /* NOTICE: here we storing id to determine will be item added or updated */ ?>
-                <input type="hidden" name="id" value="<?php echo $item['id'] ?>"/>
+                <?php /* NOTICE: here we storing id to determine will be item added or updated */ ?>
+                <input type="hidden" name="id" value="<?php echo $rel_id ?>"/>
 
                 <div class="metabox-holder" id="poststuff">
                     <div id="post-body">
                         <div id="post-body-content">
-    <?php /* And here we call our custom meta box */ ?>
-    <?php do_meta_boxes('person', 'normal', $item); ?>
+                            <?php if ($item['rel_id'] == 0) {
+                                ?>
+                                <div id="message" class="updated"><p>Please make sure that the user about to relate has already enrolled to the course</p></div>
+                                <label> Course Name :</label> <select name="course_id" >
+                                    <option value=""></option>
+                                    <?php
+                                    foreach ($courses_array as $course) {
+
+                                        echo "<option value='$course->ID'>$course->post_title </option>";
+                                    }
+                                    ?> </select>
+
+                                <br/>
+                                <label> User Name: </label> <select name="user" > 
+                                    <option value=""></option>
+                                    <?php
+                                    foreach ($users_array as $user) {
+
+                                        echo "<option value='$user->ID'>$user->user_login </option>";
+                                    }
+                                    ?></select> <br/>
+                                <?php
+                            } else {
+                                ?>
+                                <label> Course Name :</label> <?php echo $item["coursename"] ?> <br/>
+                                <label> User Name: </label> <?php echo $item["username"] ?> <br/>
+
+
+
+                                <label>   User Role: </label> <select name="user_role" id="user_role">
+                                    <option value="student" <?php echo $item["user_role"] == "student" ? "selected" : "" ?>>Learner/Student</option>
+                                    <option value="learner-manager" <?php echo $item["user_role"] == "learner-manager" ? "selected" : "" ?>>Learner manager</option>
+                                    <option value="hr-manager" <?php echo $item["user_role"] == "hr-manager" ? "selected" : "" ?>>HR Manager</option>
+                                    <option value="clientspoc" <?php echo $item["user_role"] == "clientspoc" ? "selected" : "" ?>>Clientspoc</option>
+                                    <option value="trainer" <?php echo $item["user_role"] == "trainer" ? "selected" : "" ?>>Trainer</option>
+                                    <option value="lap-assesser" <?php echo $item["user_role"] == "lap-assesser" ? "selected" : "" ?>>Lap Assesser</option>
+                                </select> <br/>
+                                <label>Reporting To : </label> 
+                                <select name="parent" id="parent">
+                                    <option value="">None</option>
+                                    <?php foreach ($users as $user) {
+                                        ?>
+                                        <option <?php echo $user["user_id"] == $item["parent_id"] ? "selected" : "" ?> value="<?php echo $user["user_id"] ?>"><?php echo $user["name"] . " - " . $user["user_role"] ?></option>
+                                    <?php }
+                                    ?>
+                                </select>
+                            <?php }
+                            ?><br/>
                             <input type="submit" value="<?php _e('Save', 'custom_table_example') ?>" id="submit" class="button-primary" name="submit">
                         </div>
                     </div>
                 </div>
             </form>
         </div>
-    <?php
-}
+        <?php
+    }
+    
